c803989ecdeed58918c1f669113ecfe13671fe83
zachharrison <zharrison100@gmail.com>
2024-11-11 19:15:52 -0800

    Fix typo in imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { Layout } from '@/components/Layout/Layout';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import { ThemeProvider } from '../context/themeContext';
+import { ThemeProvider } from '@/context/ThemeContext';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
